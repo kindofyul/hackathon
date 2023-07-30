f2094cf1e5e6e74bb1666676167068de15e6bce8
seonyul <yul0016@yonsei.ac.kr>
2023-07-30 23:59:06 +0900

    [07.30 선율]율 업무 요청 페이지 제작

--- posting/urls.py
+++ posting/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from posting.views import *
 
 app_name = 'posting'
 
 urlpatterns = [
-   
+    path('create/', posting, name="posting"),
+    path('confirmation/', confirmation, name="confirmation"),  # name 추가
 ]
