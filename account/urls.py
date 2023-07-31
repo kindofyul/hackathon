6e543251ac74ff4bc088b718a2b79f09041bef93
yg111703 <ykim1117@naver.com>
2023-07-31 15:19:01 +0900

    [07.31 연우] 홈페이지

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from account.views import *
 
 app_name = 'account'
 
 urlpatterns = [
-   
+    path('', home, name="home"),
+    path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
+    path('signup/', signup, name="signup"),
+    path('explore/', explore, name="explore"),
+    path('commission/', commission, name="commission"),
+    path('commissionneedlogin/', commissionneedlogin, name="commissionneedlogin"),
+    path('mypage/', mypage, name="mypage"),
+    path('popup/', popup_view, name='popup_view'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
+from .models import *
 
-# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    
+    if request.method == 'POST':
+        userid = request.POST['username']
+        userpw = request.POST['password']
+
+        user = auth.authenticate(request, username=userid, password=userpw)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html')
+        
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    
+    else:
+        userid = request.POST['username']
+        userpw = request.POST['password']
+        new_user = User.objects.create_user(username=userid, password=userpw)
+        auth.login(request, new_user)
+        return redirect('home')
+
+def explore(request):
+    return render(request,'explore.html')
+
+def mypage(request):
+    return render(request,'mypage.html')
+
+def commission(request):
+    return render(request,'commission.html')
+    
+def commissionneedlogin(request):
+    return redirect(commissionneedlogin)
+
+def popup_view(request):
+    return render(request, 'kakaotalk.html')
